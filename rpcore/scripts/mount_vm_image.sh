d84cadae0a78aa1042aafcd1094046ba50dfd778
Animesh Patel <animeshx.patel@intel.com>
2015-05-21 16:44:24 +0530

    vRTM-cleanup: cleanup of mount script

--- rpcore/scripts/mount_vm_image.sh
+++ rpcore/scripts/mount_vm_image.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -x
-#mountPath="/tmp/mount"
-#vhdMountPath="/tmp/vhdmnt"
+
+## Ret codes
+## 0 : Operation successful
+## 1 : Invalid input and Usage displayed
+## 2 : Image does not exist
+## 3 : unmount failed
+## 4 : guestmount failed with error code
+
 
 function unmount_vm_image() {
         echo "################ Unmounting the mount path"
@@ -9,174 +15,20 @@ function unmount_vm_image() {
         if [ ! -z $mountPathCheck ]
         then
                 umount $mountPath 2>/dev/null
-		if [ ! $? ]
+		retcode=$?
+		if [ $retcode -ne 0 ]
 		then
-			exit 1
-		fi	
-        fi
-
-        kpartx -d /dev/loop0
-        losetup -d /dev/loop0
-        qemu-nbd -d /dev/nbd0
-
-        vhdPathCheck=$(mount | grep -o "$vhdMountPath")
-        if [ ! -z $vhdPathCheck ]
-        then
-                umount $vhdMountPath 2>/dev/null
-                if [ ! $? ]
-                then
-                        echo "Unmounting $vhdMountPath Failed . . ."
-                        exit 1
-                fi
-        fi
-	
-	#kpartx -d /dev/loop0
-	#losetup -d /dev/loop0
-	#qemu-nbd -d /dev/nbd0
-	killall -9 qemu-nbd
-	killall -9 vdfuse
-
-
-	#vhdPathCheck=$(mount | grep -o "$vhdMountPath")
-	#if [ ! -z $vhdPathCheck ]
-	#then
-		#umount $vhdMountPath 2>/dev/null
-	#fi
-	
-	#umount $vhdMountPath 2>/dev/null
-        #umount $mountPath 2>/dev/null
-	#exitcode=$?
-	#echo "Unmount Exit code is -- $exitcode"
-	#if [ ! $exitcode ]
-	#then
-		#return $exitcode
-	#fi
-	#echo "Unmounted . . . . "
-	#df -h
-        rm -rf $mountPath
-        mkdir -p $mountPath
-        rm -rf $vhdMountPath
-        mkdir $vhdMountPath
-}
-
-function mount_qcow2_image() {
-        modprobe nbd
-        qemu-nbd -d /dev/nbd0
-	#echo "############ Image file path is $imagePath"
-        qemu-nbd -c /dev/nbd0 $imagePath
-	sleep 2
-        if [ -b /dev/nbd0p1 ]
-        then
-                mount /dev/nbd0p1 $mountPath
-        else
-                mount /dev/nbd0 $mountPath
+			echo "Umount for $mountPath failed with code : $retcode"
+			exit 3
+		fi
         fi
 }
 
-function mount_raw_image() {
-        temp=$(fdisk -l $imagePath | grep -o "Device")
-
-        #Check for availability of partions in the image
-        #echo "##################### ******************  $temp"
-        if [ -z $temp ];
-        then
-                #echo "############## In main If"
-                sector="0"
-        else
-                #Check for value of 'boot' field in the output to decide the value of 'start' field whether $2 or $3
-                temp=$(fdisk -l $imagePath | grep $imagePath*'1' | grep -v ":" | awk '{ print $7 }')
-                #echo "Value of temp is $temp"
-                if [ -z $temp ];
-                then
-                        #echo "################ In the nested if"
-                        sector=$(fdisk -l $imagePath | grep $imagePath*'1' | grep -v ":" | awk '{ print $2 }')
-                else
-                        #echo "######################### in the nested else"
-                        sector=$(fdisk -l $imagePath | grep $imagePath*'1' | grep -v ":" | awk '{ print $3 }')
-                fi
-        fi
-        #echo "################ sector start is $sector"
-        mount -o loop,offset=$(($sector*512)) $imagePath $mountPath
-}
-
-function mount_raw_image_duplicate() {
-	#loopDevice=$(losetup -f)
-	loopDevice="/dev/loop0"
-	kpartx -d $loopDevice
-	losetup -d $loopDevice
-	losetup $loopDevice $imagePath
-	out=$(kpartx -av $loopDevice | grep -o "$loopDevice")
-	#echo "#####%%%%%%%#######__________$out___________################"
-	if [ -z $out ]
-	then
-		mount $loopDevice $mountPath
-	else
-		partition=$(echo $loopDevice | awk -F '/' '{ print $3 }')p'1'
-		mount /dev/mapper/$partition $mountPath
-	fi
-}
-
-function mount_vhd_image() {
-        vdfuse -w -f $imagePath $vhdMountPath
-	sleep 1
-        out=$(ls $vhdMountPath | grep "Partition1")
-
-        if [ -z $out ];
-        then
-                #mount $vhdMountPath/EntireDisk $mountPath
-		imagePath="$vhdMountPath/EntireDisk"
-		#mount_raw_image
-		mount_raw_image_duplicate
-        else
-                #mount $vhdMountPath/Partition1 $mountPath
-		imagePath="$vhdMountPath/Partition1"
-		#mount_raw_image
-		mount_raw_image_duplicate
-        fi
-}
-
-usage(){
-        echo "Usage: $0 Image-File mount-path"
+usage() {
+        echo "Usage: $0 Image-File mount-path for mounting images"
+	echo "       $0 mount-path for unmounting images"		    
         exit 1
 }
-
-function check_unmount_status() 
-{
-	if ( unmount_vm_image )
-	then
-		echo "Successfully unmounted "
-	else
-		echo "Error while unmounting "
-		exit 1
-	fi
-}
-
-if [ $# -eq 1 ]
-then
-	#check_unmount_status
-	mountPath=$1
-	unmount_vm_image
-	exit 0
-fi
-
-imagePath=$1
-tmppath=$2
-mountPath="${tmppath}/mount"
-vhdMountPath="${tmppath}/vhdmnt"
-rm -rf $tmppath
-mkdir -p $mountPath
-mkdir -p $vhdMountPath
-checkVhd=$(tar tf $imagePath 2>/dev/null | grep 0.vhd)
-if [ ! -z $checkVhd ]
-then
-        parentDir=$(dirname $imagePath)
-	#echo "---------------- In the vhd check"
-        cd $parentDir
-        tar zxf $imagePath
-        imagePath=$(readlink -f 0.vhd)	
-else
-	echo ""
-fi
 
 function mount_disk_guestmount()
 {
@@ -189,69 +41,42 @@ function mount_disk_guestmount()
 	fi
 	## Proceed mounting with guestmount
 	time $guestMountBinary -a $imagePath -i $mountPath
+	retcode=$?
+	if [ $retcode -eq 0 ] ; then
+		echo "Mounted the disk image successfully"
+	else
+		echo "Guestmount failed and returned with exit code $retcode"
+		exit 4
+	fi
 }
 
-imageFormat=$(qemu-img info $imagePath  | grep "file format" | awk -F ':' '{ print $2 }' | sed -e 's/ //g')
+if [ $# -eq 1 ]
+then
+        mountPath=$1
+        unmount_vm_image
+	rm -rf `dirname $mountPath`
+        exit 0
+fi
 
-echo "################ Original image format is $imageFormat"
+imagePath=$1
+mountPath="$2/mount"
 
-case "$imageFormat" in
-   "raw")
-	echo "Mounting the raw Image."
-	#check_unmount_status
+if [ -e $imagePath ] ; then
+	echo "Image $imagePath does not exist..."
+	exit 2
+fi
+
+if [ -d $mountPath ] ; then
+	echo "mountpoint $mountPath already exists.. Unmounting it"
 	unmount_vm_image
-	#mount_raw_image
-	mount_raw_image_duplicate
-	if [ $? ]
-	then
-		echo "Successfully mounted raw image, exiting ..."
-		exit 0
-	else
-		echo "Error in mounting the image"
-		exit 1
-	fi 
-   ;;
-   "vpc")
-	echo "########### Mounting vhd Image" 
-	#check_unmount_status
-	unmount_vm_image
-	mount_vhd_image
-        if [ $? ];
-        then
-                echo "Successfully mounted vhd image, exiting ..."
-                exit 0
-        else
-                echo "Error in mounting the image"
-                exit 1
-        fi
-   ;;
-   "qcow2")
-	echo "################ Mounting qcow2 Image." 
-	#check_unmount_status
-	unmount_vm_image
-	# mount_qcow2_image
-	mount_disk_guestmount
-        if [ $? ]
-        then
-                echo "Successfully mounted qcow2 image, exiting ..."
-                exit 0
-        else
-                echo "Error in mounting the image"
-                exit 1
-        fi
-   ;;
-   *)
-	echo "############### format other than vhd, raw, qcow2 using guestmount"
-	        unmount_vm_image
-        # mount_qcow2_image
-        mount_disk_guestmount
-        if [ $? ]
-        then
-                echo "Successfully mounted qcow2 image, exiting ..."
-                exit 0
-        else
-                echo "Error in mounting the image"
-                exit 1
-        fi
-esac
+else
+	echo "mountpoint $mountPath does not exist, creating one"
+	mkdir -p $mountPath
+fi
+
+imageFormat=$(qemu-img info $imagePath | grep "file format" | awk -F ':' '{ print $2 }' | sed -e 's/ //g')
+
+echo "Disk image format : $imageFormat"
+mount_disk_guestmount
+exit 0
 
