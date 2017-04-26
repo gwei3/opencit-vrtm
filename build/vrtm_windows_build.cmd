e7c08ffbca0b6282a8ce413ea34dafc3c474c303
Adolfo <adolfo.v.aguayo@intel.com>
2017-04-26 16:04:00 -0500

    v3.1-rc1+next+Bug#7111-ext2fsd_needs_to_be_moved_to_external_artifacts: Bug#7111-pulling Ext2Fsd-0.62.exe into third-party folder

--- build/vrtm_windows_build.cmd
+++ build/vrtm_windows_build.cmd
@@ -39,6 +39,15 @@ GOTO:EOF
   echo. "%vrtm_home%\third party\pthread\dll\%1\pthreadVC2.dll"
   copy "%vrtm_home%\third party\pthread\dll\%1\pthreadVC2.dll" "%vrtm_home%\bin" /y
   
+  set group_id="net.sourceforge.ext2fsd"
+  set artifact_id="ext2fsd"
+  set version="0.62"
+  set packaging="exe"
+  cd..
+  SET vrtm_home=%cd%\vrtm
+  cd build\
+  call mvn dependency:get -DgroupId=%group_id% -DartifactId=%artifact_id% -Dversion=%version% -Dpackaging=%packaging% -Ddest="%vrtm_home%\third party\Ext2Fsd-0.62.exe"
+  
   mkdir "%vrtm_home%\prerequisites"
   echo. "%vrtm_home%\third party\Ext2Fsd-0.62.exe"
   copy "%vrtm_home%\third party\Ext2Fsd-0.62.exe" "%vrtm_home%\prerequisites" /y
